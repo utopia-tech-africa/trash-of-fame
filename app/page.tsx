4af4d8b466922362b768bc15c6382049ae4ec5b8
Linda Mensah <lindaakmensah@gmail.com>
2026-04-23 17:14:12 +0000

    feat: add all other sections

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { About } from "@/components/about";
 import { Footer } from "@/components/footer";
 import { Mission } from "@/components/mission";
 import { Contact } from "@/components/contact/contact";
+import { Problem } from "@/components/problem";
+import { Blogs } from "@/components/blogs";
 
 export default function Home() {
   return (
@@ -11,7 +13,9 @@ export default function Home() {
       <Navbar />
       <Hero />
       <About />
+      <Problem />
       <Mission />
+      <Blogs />
       <Contact />
       <Footer />
     </div>
